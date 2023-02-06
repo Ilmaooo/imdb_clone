e7cc23206d1e172c19f6c711630f6b532a47b65c
Ilmaooo <ilmaogresevic1@gmail.com>
2023-02-06 18:33:28 +0100

    navbar created

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,12 @@
 export default function page() {
   return (
-    <div className="max-w-6xl mx-auto space-y-4">
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
       <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-stop destination for
+        all your movie-related needs.
+      </p>
       <p>
         Our website is designed to provide you with a comprehensive database of
         movies from all around the world, along with the latest news, reviews,
